16ad38509a8d87a77e9db18f80b312434a8298f7
Yashvardhan Goel <yash2002goel@gmail.com>
2025-05-31 13:49:45 +0530

    feat: enhance pokemon data parsing with role classification and refactor data structures

--- backend/app/utils/parse_pokemon_data.py
+++ backend/app/utils/parse_pokemon_data.py
@@ -1,19 +1,32 @@
 from app.service.pokemon import get_pokemon_service
 
 
+def get_roles(pokemon_stats_dict: dict) -> list:
+    roles = []
+    if pokemon_stats_dict["speed"] >= 90:
+        roles.append("fast")
+    if pokemon_stats_dict["attack"] >= 85:
+        roles.append("physical-attacker")
+    if pokemon_stats_dict["special-attack"] >= 90:
+        roles.append("special-attacker")
+    if pokemon_stats_dict["defense"] >= 90 or pokemon_stats_dict["special-defense"] >= 90:
+        roles.append("tank")
+    if pokemon_stats_dict["hp"] >= 80 and (pokemon_stats_dict["defense"] >= 80 or pokemon_stats_dict["special-defense"] >= 80):
+        roles.append("defensive")
+    return roles
+
+
 async def parse_pokemon_data(pokemon_name: str) -> dict:
     service = await get_pokemon_service()
     pokemon_data = await service.get_pokemon_data(pokemon_name)
 
     abilities = pokemon_data.get("abilities", [])
-    abilities_list = []
+    abilities_dict = {}
     for ability in abilities:
         ability_name = ability.get("ability", {}).get("name", "")
         hidden_status = ability.get("is_hidden", False)
-        abilities_list.append({
-            "ability_name": ability_name,
-            "hidden_status": hidden_status
-        })
+
+        abilities_dict[ability_name] = hidden_status
 
     moves = pokemon_data.get("moves", [])
     moves_list = []
@@ -28,14 +41,13 @@ async def parse_pokemon_data(pokemon_name: str) -> dict:
         pokemon_types_list.append(type_name)
 
     pokemon_stats = pokemon_data.get("stats", [])
-    pokemon_stats_list = []
+    pokemon_stats_dict = {}
     for stat in pokemon_stats:
         stat_name = stat.get("stat", {}).get("name", "")
         stat_value = stat.get("base_stat", None)
-        pokemon_stats_list.append({
-            "stat_name": stat_name,
-            "stat_value": stat_value
-        })
+        pokemon_stats_dict[stat_name] = stat_value
+
+    roles = get_roles(pokemon_stats_dict)
 
     base_experience = pokemon_data.get("base_experience", None)
     height = pokemon_data.get("height", None)
@@ -46,15 +58,16 @@ async def parse_pokemon_data(pokemon_name: str) -> dict:
     pokemon_weight = pokemon_data.get("weight", None)
     
     data = {
-        "abilities": abilities_list,
+        "abilities": abilities_dict,
         "moves": moves_list,
         "types": pokemon_types_list,
-        "stats": pokemon_stats_list,
+        "stats": pokemon_stats_dict,
         "base_experience": base_experience,
         "height": height,
         "pokemon_id": pokemon_id,
         "pokemon_species": pokemon_species,
         "pokemon_weight": pokemon_weight,
+        "role_type": roles,
     }
 
     return data
